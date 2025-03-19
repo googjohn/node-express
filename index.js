569a8c79bbf541f74e165df06f31766d7504a29f
googjohn <120353235+googjohn@users.noreply.github.com>
2025-03-19 20:50:48 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/* declare/define express by calling using require() or import on common js and type module, respectively */
+const express = require('express');
+// call express to create instance
+const app = express();
+console.log(app);
+const PORT = 5000;
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to http://localhost:${PORT}`)
+})
